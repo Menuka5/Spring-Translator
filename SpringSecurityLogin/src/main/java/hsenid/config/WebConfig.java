030d16faab1f80ddf82e0dc28ca8ef8bfe6fbaac
Menuka <menukaishan5@gmail.com>
2017-05-03 18:13:52 +0530

    User Role authentication adding

--- SpringSecurityLogin/src/main/java/hsenid/config/WebConfig.java
+++ SpringSecurityLogin/src/main/java/hsenid/config/WebConfig.java
@@ -4,32 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("hsenid.web")
-public class WebConfig extends WebMvcConfigurerAdapter {
+@ComponentScan(basePackages = "hsenid")
+public class WebConfig {
     @Bean
-    public ViewResolver viewResolver(){
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/");
-        resolver.setSuffix(".jsp");
-        resolver.setExposeContextBeansAsAttributes(true);
-        return resolver;
-    }
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
 
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
-        configurer.enable();
-    }
-
-    @Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        return viewResolver;
     }
 }
--- SpringSecurityLogin/src/main/webapp/WEB-INF/home.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello World!hyhfgh</h2>
-</body>
-</html>
